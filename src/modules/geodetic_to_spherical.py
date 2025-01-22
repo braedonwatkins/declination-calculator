3b9005c4d15d9914d86524fdcd88d5ade0ecb94f
braedonwatkins <watkins.braedon@gmail.com>
2025-01-22 02:41:55 -0500

    change geodetic vars to fit technical report better
    changing the vars this way makes it easier to follow the report

--- src/modules/geodetic_to_spherical.py
+++ src/modules/geodetic_to_spherical.py
@@ -6,14 +6,14 @@ e2 = 2 * f - f**2  # square of eccentricity (deviation from a sphere)
 
 
 # TODO: Check for any math footguns div by 0, inefficiencies, etc.
-def geodetic_to_spherical(lat: float, alt: float) -> tuple[float, float]:
+def geodetic_to_spherical(lat: float, h: float) -> tuple[float, float]:
     """
     Converts geodetic coordinates to geocentric spherical radius and latitude
 
 
     Parameters:
         lat (float): Latitude in degrees
-        alt (float): altitude in meters
+        h (float): altitude in meters
 
     Returns:
         (r, geocentric_lat) (tuple(float)): Geocentric spherical radius and latitude
@@ -21,20 +21,16 @@ def geodetic_to_spherical(lat: float, alt: float) -> tuple[float, float]:
 
     lat_rad = math.radians(lat)  # phi
 
-    # cache trig functions to avoid recalculating
-    sin_lat_rad = math.sin(lat_rad)
-    cos_lat_rad = math.cos(lat_rad)
-
     # Prime vertical radius i.e. distance from center due to curvature
-    N = a / math.sqrt(1 - e2 * sin_lat_rad**2)
+    R = a / math.sqrt(1 - e2 * math.sin(lat_rad) ** 2)
 
     # just in case, here's the other geocentric coords
-    # x = (N + alt) * cos_lat_rad * cos_lon_rad
-    # y = (N + alt) * cos_lat_rad * sin_lon_rad
-
-    z = ((1 - e2) * N + alt) * sin_lat_rad
-    p = (N + alt) * cos_lat_rad  # eq to math.sqrt(x**2 + y**2)
+    # x = (N + h) * cos_lat_rad * cos_lon_rad
+    # y = (N + h) * cos_lat_rad * sin_lon_rad
+    p = (R + h) * math.cos(lat_rad)  # eq to math.sqrt(x**2 + y**2)
+    z = (R * (1 - e2) + h) * math.sin(lat_rad)
     r = math.sqrt(p**2 + z**2)
+
     geocentric_lat = math.asin(z / r)
 
     return (r, geocentric_lat)
