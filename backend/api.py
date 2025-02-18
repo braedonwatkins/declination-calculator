49a1d5dc3d0df67e17f93b3c678bfeb693706fda
braedonwatkins <watkins.braedon@gmail.com>
2025-02-18 10:55:23 -0500

    bulk calculate endpoint
    working at roughly 1000 points / 1 second

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,20 @@
-from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import numpy as np
+import math
+
 from .calc.core import field_vector
 
+latMin, latMax = -90, 90
+lonMin, lonMax = -180, 180
+
 app = FastAPI()
+
+
+@app.get("/")
+async def default():
+    return "Ready 3 Biznes"
 
 
 class Test(BaseModel):
@@ -14,12 +24,32 @@ class Test(BaseModel):
     year: float
 
 
-@app.get("/")
-async def default():
-    return "Ready 3 Biznes"
-
-
 @app.post("/point-calc")
 async def point_calc(req: Test):
-    test = field_vector(req.lat, req.lon, req.h, req.year)
-    return {"RESULT": test}
+    vector = field_vector(req.lat, req.lon, req.h, req.year)
+    return {"message": "success", "vector": vector}
+
+
+class Test2(BaseModel):
+    num_points: int
+    h: float
+    year: float
+
+
+@app.post("/vector-field")
+async def vector_field(req: Test2):
+    num_points = int(math.sqrt(req.num_points))
+
+    lon_points = np.linspace(lonMin, lonMax, num_points)
+    lat_points = np.linspace(latMin, latMax, num_points)
+
+    vectors = []
+
+    for lon in lon_points:
+        for lat in lat_points:
+            # print(lat, lon)
+            vec = field_vector(lat, lon, req.h, req.year)
+            print(vec)
+            vectors.append(vec)
+
+    return {"message": "success", "vectors": vectors}
